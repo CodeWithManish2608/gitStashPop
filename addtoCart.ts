2938071e46b36856c27725170ce251d15e5bf205
“Manish <manish.badgujar500@gmail.com>
2026-08-25 08:28:17 +0530

    I am Manish

--- addtoCart.ts
+++ addtoCart.ts
@@ -4,3 +4,10 @@ console.log("Add to Cart Test");
 
 // Remove from Cart
 console.log("Remove from Cart Test");
+
+//Manish
+console.log("I am Manish and I am Adding product to cart...");
+console.log("Product Added Successfully")
+
+//Rahul
+//code
