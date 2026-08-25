2061af1fd508e87848e16e1f8a764f898efdeeec
“Manish <manish.badgujar500@gmail.com>
2026-08-25 08:18:25 +0530

    I am rahul

--- addtoCart.ts
+++ addtoCart.ts
@@ -4,3 +4,11 @@ console.log("Add to Cart Test");
 
 // Remove from Cart
 console.log("Remove from Cart Test");
+
+
+
+
+
+
+//RahulCode
+console.log('I am rahul I am removing items from cart also checked payment')
